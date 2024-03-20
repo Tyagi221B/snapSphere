6ed2758adcebc786702b78e4c958b4e37a887ec1
Asmit tyagi <ashmittyagigms@gmail.com>
2024-03-20 19:05:07 +0530

    configure Shadcn , make Signup form . (Shadcn is awesome wow!!)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './globals.css';
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-    return <div>
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    </div>;
+	return (
+		<main className="flex h-screen">
+			<Routes>
+				{/* public routes */}
+				<Route element={<AuthLayout />}>
+					<Route path="/sign-in" element={<SigninForm />} />
+					<Route path="/sign-up" element={<SignupForm />} />
+				</Route>
+				{/* private routes */}
+				<Route element={<RootLayout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</main>
+	);
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
