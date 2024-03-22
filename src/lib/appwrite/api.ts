b9f00c24565a5f04471518452dc4130b65ba7e9f
Asmit tyagi <ashmittyagigms@gmail.com>
2024-03-22 20:32:23 +0530

    add createPost , add getFilePreview , add uploadFIle,

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID, Query } from "appwrite";
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -71,17 +71,17 @@ export async function signInAccount(user: {
     }
 }
 
-// export async function getAccount(){
-//     try {
-//         const currentAccount = await account.get();
-//         return currentAccount;
-//     } catch (error) {
-//         console.log('Error in getAccount')
-//     }
-// }
-export async function getCurrentUser() {
+export async function getAccount() {
     try {
         const currentAccount = await account.get();
+        return currentAccount;
+    } catch (error) {
+        console.log('Error in getAccount')
+    }
+}
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await getAccount();
 
         if (!currentAccount) {
             throw Error;
@@ -104,11 +104,100 @@ export async function getCurrentUser() {
     }
 }
 
-export async function signOutAccount(){
+export async function signOutAccount() {
     try {
         const session = await account.deleteSession("current");
         return session;
     } catch (error) {
         console.log('Error in signOutAccount function in api.ts')
+    }
+}
+
+export async function createPost(post: INewPost) {
+    try {
+        //upload file to appwrite storage
+        const uploadedFile = await uploadFile(post.file[0]);
+        if (!uploadedFile) throw Error;
+
+        //get file URL
+
+        const fileUrl = getFilePreview(uploadedFile.$id);
+        if (!fileUrl) {
+            await deleteFile(uploadedFile.$id);
+            throw Error;
+        }
+
+        //convert tags into array
+        const tags = post.tags?.replace(/ /g, "").split(",") || [];
+
+        //Create post
+
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            ID.unique(),
+            {
+                creator: post.userId,
+                caption: post.caption,
+                imageUrl: fileUrl,
+                imageId: uploadedFile.$id,
+                location: post.location,
+                tags: tags,
+            }
+        )
+
+        if (!newPost) {
+            await deleteFile(uploadedFile.$id);
+            throw Error;
+        }
+
+        return newPost;
+
+
+    } catch (error) {
+        console.log("Error is in createPost function in api.ts file", error);
+
+    }
+}
+
+export async function deleteFile(FileId: string) {
+    try {
+        await storage.deleteFile(appwriteConfig.storageId, FileId);
+        return { status: "ok" }
+    } catch (error) {
+        console.log("Error is in deleteFile function in api.ts", error);
+
+    }
+}
+
+export function getFilePreview(fileId: string) {
+    try {
+        const fileUrl = storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            "top",
+            100
+        );
+        if (!fileUrl) throw Error;
+        return fileUrl;
+    } catch (error) {
+        console.log("Error is in getFilePreview function in api.ts", error);
+
+    }
+}
+
+export async function uploadFile(file: File) {
+    try {
+        const uploadedFile = await storage.createFile(
+            appwriteConfig.storageId,
+            ID.unique(),
+            file
+        );
+
+        return uploadedFile;
+    } catch (error) {
+        console.log("Error is in uploadFile function in api.ts", error)
     }
 }
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,12 @@
-import { INewUser } from "@/types"
+import { INewPost, INewUser } from "@/types"
 import {
     useQuery,
     useMutation,
     useQueryClient,
     useInfiniteQuery
 } from "@tanstack/react-query"
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api"
+import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
+import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api"
 
 export const useCreateUserAccount = () =>{
     return useMutation({
@@ -24,6 +25,18 @@ export const useSignInAccount = () =>{
 export const useSignOutAccount = () =>{
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+export const useCreatePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey:[QUERY_KEYS.GET_RECENT_POSTS],
+            })
+        }
     })
 }
 
