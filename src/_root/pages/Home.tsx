96c10704191e3ae8c3d933ee375516cdca070fbc
Asmit tyagi <ashmittyagigms@gmail.com>
2024-03-27 15:09:11 +0530

    debugg

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (
+          {isPostLoading || isErrorPosts && !posts ? (
             <Loader/>
           ): (
             <ul 
