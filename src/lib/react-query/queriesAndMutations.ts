b15f2c8cb7bf2898ca5c41e58e5b38f640df6ca3
Asmit tyagi <ashmittyagigms@gmail.com>
2024-03-27 20:23:56 +0530

    debugged

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -174,25 +174,8 @@ export const useDeletePost = () => {
 }
 
 
-// export const useGetPosts = () => {
-//     return useInfiniteQuery({
-//         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-//         queryFn: getInfinitePosts as any,
-//         getNextPageParam: (lastPage: any) => {
-//             if (lastPage && lastPage.documents.length === 0) {
-//                 return null;
-//             }
-
-//             const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
-//             return lastId;
-//         },
-//     });
-// };
 
 export const useGetPosts = () => {
-    
-    const initialPageParam = 1;
-
     return useInfiniteQuery({
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
         queryFn: getInfinitePosts as any,
@@ -204,7 +187,6 @@ export const useGetPosts = () => {
             const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
             return lastId;
         },
-        initialPageParam, 
     });
 };
 
